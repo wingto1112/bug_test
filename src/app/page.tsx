4209e05355b1abfe4b4549a089b64c395f0a453b
Ben To <hon.to@students.plymouth.ac.uk>
2023-07-23 18:39:14 +0800

    Remove hardcoded data, fetching data from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import {
   TableContainer,
   TableRow,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 
 type MyValue = {
   id: number;
@@ -23,37 +23,32 @@ type MyValue = {
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const v: MyValue[] = [
-    { id: 1, name: "John", value: 45, group: "A" },
-    { id: 2, name: "Sarah", value: 72, group: "B" },
-    { id: 3, name: "David", value: 17, group: "A" },
-    { id: 4, name: "Emily", value: 94, group: "C" },
-    { id: 5, name: "Michael", value: 28, group: "B" },
-    { id: 6, name: "Jessica", value: 62, group: "C" },
-    { id: 7, name: "Andrew", value: 39, group: "A" },
-    { id: 8, name: "Grace", value: 81, group: "B" },
-    { id: 9, name: "Anthony", value: 12, group: "A" },
-    { id: 10, name: "Rachel", value: 76, group: "C" },
-    { id: 11, name: "Thomas", value: 55, group: "B" },
-    { id: 12, name: "Olivia", value: 89, group: "A" },
-    { id: 13, name: "Daniel", value: 33, group: "C" },
-    { id: 14, name: "Madison", value: 70, group: "B" },
-    { id: 15, name: "Nicholas", value: 25, group: "A" },
-    { id: 16, name: "Lauren", value: 60, group: "C" },
-    { id: 17, name: "Christopher", value: 47, group: "B" },
-    { id: 18, name: "Ava", value: 97, group: "A" },
-    { id: 19, name: "Tyler", value: 20, group: "C" },
-    { id: 20, name: "Sophia", value: 84, group: "B" },
-  ];
 
-  const limit = 10;
-  const [rows, setRows] = React.useState<MyValue[]>(v.slice(0, limit));
+  const [rows, setRows] = React.useState<MyValue[]>([]);
 
   const handleChange = (page: number) => {
-    const start = limit * page;
-    const end = start + limit;
-    setRows(v.slice(start, end));
+    updateRows(page)
   };
+
+  const updateRows = async (page: number) => {
+    try {
+      const res = await fetch(
+        `http://localhost:3001/member?_page=${page}&_limit=10`,
+        {
+          method: 'GET',
+        }
+      );
+      const members = await res.json();
+      setRows(members);
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    updateRows(1)
+  }, [])
 
   return (
     <main className={styles.main}>
@@ -86,7 +81,7 @@ export default function Home() {
           <Button
             variant="text"
             onClick={() => {
-              handleChange(0);
+              handleChange(1);
             }}
           >
             First
@@ -94,7 +89,7 @@ export default function Home() {
           <Button
             variant="text"
             onClick={() => {
-              handleChange(1);
+              handleChange(2);
             }}
           >
             Second
